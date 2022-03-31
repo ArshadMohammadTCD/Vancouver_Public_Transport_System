fb226b4083af3442fbf9c989a54372a7757416c7
Arshad Rehman Mohammed <mohammar@tcd.ie>
2022-03-31 11:02:29 +0100

    Shortest Path initialised

---
+++ Edge.java
@@ -0,0 +1,9 @@
+
+public class Edge {
+		double weight;
+		int	destVertex;
+		Edge(int destVertex, double weight){
+			this.destVertex = destVertex;
+			this.weight = weight;
+		}
+	}
--- ShortestPath.java
+++ ShortestPath.java
@@ -1,7 +1,86 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class ShortestPath {
-	
-	
-	
-	
+
+
+	ShortestPath(String stops, String transfers, String stopTimes)
+	{	
+		parseStops(stops);
+		parseTransfers(transfers);
+		parseStopTimes(stopTimes);
+
+	}
+	private void parseStopTimes(String stopTimes) {
+		if (stopTimes == null) {
+			return;
+		}
+		File a = new File(stopTimes);
+		if (a.length() != 0){
+			Scanner input = null;
+			try {
+				input = new Scanner(a);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			while (input.hasNextLine()) {
+				String times = input.nextLine();
+				String[] data = times.split(",");
+				System.out.println(data[0]);
+			}
+			
+			
+		}
+		
+		
+		
+		
+	}
+	private void parseTransfers(String transfers) {
+		
+	}
+
+
+
+
+
+	private void parseStops(String stops) 
+	{
+		int numOfVertices = 0 ;
+		if (stops == null) {
+			return;
+		}
+		File a = new File(stops);
+		if (a.length() != 0){
+
+
+			Scanner input = null;
+
+
+
+			try {
+				input = new Scanner(a);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+
+
+	}
+
+
+
+
+	public static void main(String[] args) {
+		ShortestPath SP = new ShortestPath("src/stops.txt","transfers.txt", "src/stop_times.txt");
+		
+
+	}
 }
