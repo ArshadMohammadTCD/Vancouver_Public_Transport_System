d2b3ffe8f8122860731a94673ce3cabf90e007ae
Arshad Rehman Mohammed <mohammar@tcd.ie>
2022-04-06 17:00:18 +0100

    Added Queue from Princeton

--- StdOut.java
+++ StdOut.java
@@ -292,13 +292,5 @@ public final class StdOut {
      *
      * @param args the command-line arguments
      */
-    public static void main(String[] args) {
-
-        // write to stdout
-        StdOut.println("Test");
-        StdOut.println(17);
-        StdOut.println(true);
-        StdOut.printf("%.6f\n", 1.0/7.0);
-    }
 
 }
