6eb272b5f81a6c3e1d893e9fcb34180becbc1982
Arshad Rehman Mohammed <mohammar@tcd.ie>
2022-04-07 13:33:21 +0100

    Base code parse TST search

--- ShortestPath.java
+++ ShortestPath.java
@@ -269,30 +269,32 @@ public class ShortestPath {
 
 	public static void main(String[] args) {
 
-		TST<Integer> st = new TST<Integer>();
-		System.out.println("Its doing something!");
-//        for (int i = 0; !StdIn.isEmpty(); i++) {
-////        	System.out.println("Its doing something2!");
-        String word = "short";
-        st.put("short", 0);
-        st.put("shoe", 1); 
-        st.put("shame", 1);
-        
-        
-        System.out.println("keysWithPrefix(\"sh\")");
-        for (String s : st.keysWithPrefix("sh"))
-            System.out.println(s);
-        
-        
-        System.out.println("keysWithPrefix(\"shor\")");
-        for (String s : st.keysWithPrefix("shor"))
-            System.out.println(s);
-        
-
-        System.out.println("keysThatMatch(\"shoe\")");
-        for (String s : st.keysThatMatch("shoe"))
-            System.out.println(s);
-    
+//		TST<Integer> st = new TST<Integer>();
+//		System.out.println("Its doing something!");
+////        for (int i = 0; !StdIn.isEmpty(); i++) {
+//////        	System.out.println("Its doing something2!");
+//        String word = "short";
+//        st.put("short", 0);
+//        st.put("shoe", 1); 
+//        st.put("shame", 1);
+//        
+//        
+//        System.out.println("keysWithPrefix(\"sh\")");
+//        for (String s : st.keysWithPrefix("sh"))
+//            System.out.println(s);
+//        
+//        
+//        System.out.println("keysWithPrefix(\"shor\")");
+//        for (String s : st.keysWithPrefix("shor"))
+//            System.out.println(s);
+//        
+//
+//        System.out.println("keysThatMatch(\"shoe\")");
+//        for (String s : st.keysThatMatch("shoe"))
+//            System.out.println(s);
+		
+		TSTSearch st = new TSTSearch("src/stops.txt");
+		
 		}
 		
 		
--- TSTSearch.java
+++ TSTSearch.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TSTSearch {
 	
@@ -10,9 +14,32 @@ public class TSTSearch {
 	}
 
 	private void parseStops(String stopsFilename) {
-		
+		if (stopsFilename == null) {
+			return;
+		}
+		File a = new File(stopsFilename);
+		if (a.length() != 0){
+			Scanner input = null;
+			try {
+				input = new Scanner(a);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String firstLine = input.nextLine();
+			//			System.out.println(firstLine);
+			while (input.hasNextLine()) {
+				String times = input.nextLine();
+				String[] data = times.split(",");
+				System.out.println(data[2]);
+
+			}	
+
+
+		}	
 		
 	}
 	
 	
 }
+//git add -A; git commit -m "made changes to parseStops in the TST Class p1" ; git push origin master
